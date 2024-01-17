d01f6fbaae46251e2e86e1b5401a4dfcb18538bd
Chengyi Tang <145063160+chtang-hmc@users.noreply.github.com>
2024-01-17 13:52:37 -0800

    Update HW1C.py

--- HW1C.py
+++ HW1C.py
@@ -1,1 +1,32 @@
+import numpy as np
+import scipy as sp
 
+def pi2by6(accuracy):
+
+# Inputs:
+#
+# accuracy: A number between 0 and 1 denoting the percent error in the result.
+# 
+# Outputs:
+#  
+# Returns (value, N): 
+#   value is an approximation to pi square over six.
+#   N is the number of terms used in the sum formula.
+
+
+def energy(terms, b):
+
+# Inputs:
+#
+# terms: a positive non-zero number determining the desired precision 
+# as determined by the number of terms used in the sums above.
+# b: The dimensionless quantity hbar omega over k_B T
+#
+# Outputs:
+#
+# Returns a floating point number that is the average energy per hbar omega
+#
+# Notice how we have ensured that the two arguments and output are all 
+# dimensionless quantities, measured in the appropriate scales in the problem.
+
+  
